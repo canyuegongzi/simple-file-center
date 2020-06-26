aadb049ba2d142cd8225e2d4bf8dfe2d261ea0ba
Marvin <yongfeide123@gmail.com>
2020-06-26 19:00:23 +0800

    feat:持续集成部署测试4145455

--- dist/common/shared/decorators/roles.decorator.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
-exports.Roles = (...roles) => common_1.SetMetadata('roles', roles);
